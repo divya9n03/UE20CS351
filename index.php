7b7aa671c2df6c2dbae965818d5b3201d3c05c84
divya9n03 <129599333+divya9n03@users.noreply.github.com>
2023-04-18 10:10:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <label for="id">id</label>
     <input type="text" name="id">
 
-    <label for="number">number</label>
+    <label for="number">Number</label>
     <input type="number" name="number">
     <button type="submit" name="submit">submit</button>
   </form>
